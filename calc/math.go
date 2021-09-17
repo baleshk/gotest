57729642280c501f62f1c0ee18b2d203aaae4f78
baleshk <balesh@google.com>
2021-09-17 22:29:02 +0530

    add requires atleast two parameters

--- calc/math.go
+++ calc/math.go
@@ -1,9 +1,14 @@
 package calc
 
-func Add(number ...int) int {
+import "errors"
+
+func Add(number ...int) (int, error) {
 	s := 0
+	if len(number) < 2 {
+		return 0, errors.New("numbers provided are less than two")
+	}
 	for _, n := range number {
 		s += n
 	}
-	return s
+	return s, nil
 }
